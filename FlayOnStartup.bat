0e11e6f9d07ee6680a081f3ba45bd74746280468
kamoru <kamoru@kamoru-home>
2016-11-02 22:49:37 +0900

    off jmx auth

--- FlayOnStartup.bat
+++ FlayOnStartup.bat
@@ -5,7 +5,7 @@ setlocal
 set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote"
 set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote.port=58869"
 set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote.ssl=false"
-set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote.authenticate=true"
+set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote.authenticate=false"
 set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote.access.file=jmxremote.access"
 set "JAVA_OPTS=%JAVA_OPTS% -Dcom.sun.management.jmxremote.password.file=jmxremote.password"
 set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF-8"
