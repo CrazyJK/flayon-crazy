d54acdab010173aa1c157f442aaf00600f44a4a9
kamoru <kamoru@kamoru-home>
2017-02-14 21:17:03 +0900

    adjust log

--- src/main/java/jk/kamoru/flayon/base/security/UserDetailsServiceImpl.java
+++ src/main/java/jk/kamoru/flayon/base/security/UserDetailsServiceImpl.java
@@ -21,6 +21,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
+		log.info("loadUserByUsername [{}]", username);
+		
 		User found = null;
 
 		List<User> foundUsers = userRepository.findByName(username);
