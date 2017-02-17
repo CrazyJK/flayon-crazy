92c0d7a54791d5e7c27cffd297bd6c1a115dc9ee
kamoru <kamoru@kamoru-mint>
2017-02-17 15:18:39 +0900

    make war archive

--- src/main/java/jk/kamoru/flayon/FlayOnApplication.java
+++ src/main/java/jk/kamoru/flayon/FlayOnApplication.java
@@ -2,9 +2,16 @@ package jk.kamoru.flayon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlayOnApplication {
+public class FlayOnApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FlayOnApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlayOnApplication.class, args);
