9b1f060d2f0d604a66fdc68a3cdc0d884c105c00
kamoru <kamoru@kamoru-home>
2017-03-13 15:56:23 +0900

    change HttpGet

--- src/main/java/jk/kamoru/flayon/crazy/video/util/TistoryRSSReader.java
+++ src/main/java/jk/kamoru/flayon/crazy/video/util/TistoryRSSReader.java
@@ -1,6 +1,5 @@
 package jk.kamoru.flayon.crazy.video.util;
 
-import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +8,10 @@ import java.util.Map;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -25,6 +28,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TistoryRSSReader {
 
+	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+
 	/**
 	 * rss url을 읽어 item 태그(각 페이지)를 {@link jk.kamoru.flayon.crazy.video.domain.TistoryGraviaItem TistoryItem}의 생성자로 넣어 List 반환
 	 * @param rssUrl
@@ -34,10 +39,18 @@ public class TistoryRSSReader {
 		log.info("read rss ; {}", rssUrl);
 		List<TistoryGraviaItem> items = new ArrayList<>();
 		
-		try (InputStream is = rssUrl.openConnection().getInputStream()) {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(rssUrl.toString());
+
+		// add request header
+		request.addHeader("User-Agent", USER_AGENT);
+
+		try {
+			HttpResponse response = client.execute(request);
+			
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document doc = builder.parse(is);
+			Document doc = builder.parse(response.getEntity().getContent());
 			doc.getDocumentElement().normalize();
 			log.debug("Root element :" + doc.getDocumentElement().getNodeName());
 			
@@ -50,8 +63,9 @@ public class TistoryRSSReader {
 			}
 			log.info("found item {}", items.size());
 		} catch (Exception e) {
-			log.error("Fail to Tistory Rss read", e);
+			e.printStackTrace();
 		}
+
 		return items;
 	}
 	
