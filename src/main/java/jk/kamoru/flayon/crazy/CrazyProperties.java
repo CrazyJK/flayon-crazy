74e2ba4db8233a687bf906c8c77f65e7e649b8f0
kamoru <kamoru@kamoru-mint>
2017-07-24 16:31:01 +0900

    remove base line property

--- src/main/java/jk/kamoru/flayon/crazy/CrazyProperties.java
+++ src/main/java/jk/kamoru/flayon/crazy/CrazyProperties.java
@@ -6,48 +6,48 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class CrazyProperties {
 
-	/* Local properties */
+    /* Local properties */
 
-	/** 비디오 아카이브 경로 */ 			@Value("${path.video.archive}")			protected String	      ARCHIVE_PATH;
-	/** base video path in properties */	@Value("${path.video.storage}") 		protected String 	      STORAGE_PATH;
-	/** 다운받은 후보 비디오 경로 */		@Value("${path.video.candidate}") 		protected String[] 	    CANDIDATE_PATHS;
-	/** 비디오 Stage 경로 */				@Value("${path.video.stage}") 			protected String[] 	        STAGE_PATHS;
-	/** 비디오 cover 경로 */				@Value("${path.video.cover}") 			protected String 	        COVER_PATH;
-	/** Torrent seed 경로 */				@Value("${path.video.seed}") 			protected String 	         SEED_PATH;
-	
-	/** 특정 파일 이동 정보 */ 				@Value("${path.move.file}")				protected String[] 	    MOVE_FILE_PATHS;
-	/** 다운받은 이미지의 이름 바꿀 경로 */ @Value("${path.sora.pictures}")			protected String[] 	SORA_PICTURES_PATHS;
-	/** 이미지 파일 경로 */					@Value("${path.image.storage}") 		protected String[] 	        IMAGE_PATHS;
-	/** 토렌트 파일 경로 */					@Value("${path.video.torrent}") 		protected String 	      TORRENT_PATH;
-	/** 백업 경로 */						@Value("${path.backup}") 				protected String	       BACKUP_PATH;
+    /** video archive path */                    @Value("${path.video.archive}")         protected String         ARCHIVE_PATH;
+    /** base video path in properties */         @Value("${path.video.storage}")         protected String         STORAGE_PATH;
+    /** video path of downloaded file */         @Value("${path.video.candidate}")       protected String[]     CANDIDATE_PATHS;
+    /** video Stage path */                      @Value("${path.video.stage}")           protected String[]         STAGE_PATHS;
+    /** video cover path */                      @Value("${path.video.cover}")           protected String           COVER_PATH;
+    /** Torrent seed path */                     @Value("${path.video.seed}")            protected String            SEED_PATH;
 
-	/** 비디오 플레이어 실행 파일 */		@Value("${app.video-player}") 			protected String   	PLAYER;
-	/** 자막 편집 실행 파일 */				@Value("${app.subtitles-editor}") 		protected String   	EDITOR;
+    /** specific file moving info */             @Value("${path.move.file}")             protected String[]     MOVE_FILE_PATHS;
+    /** managed picture path */                  @Value("${path.sora.pictures}")         protected String[] SORA_PICTURES_PATHS;
+    /** image file path */                       @Value("${path.image.storage}")         protected String[]         IMAGE_PATHS;
+    /** torrent file path */                     @Value("${path.video.torrent}")         protected String         TORRENT_PATH;
+    /** backup path */                           @Value("${path.backup}")                protected String          BACKUP_PATH;
 
-	/* Common Properties */
-	
-	/** minimum rank in properties */		@Value("${rank.minimum}") 				protected Integer  	 MIN_RANK;
-	/** maximum rank in properties */		@Value("${rank.maximum}") 				protected Integer  	 MAX_RANK;
-	/** baseline rank in properties */		@Value("${rank.baseline}")  			protected int 	 	BASE_RANK;
-	/** 전체 비디오의 크기 제한 */			@Value("${size.video.storage}")  		protected int 	 	MAX_ENTIRE_VIDEO;
+    /** video player exec */                     @Value("${app.video-player}")           protected String   PLAYER;
+    /** subtitles editer */                      @Value("${app.subtitles-editor}")       protected String   EDITOR;
 
-	/** 스튜디오를 찾지 않을 특정 품번 */	@Value("${parse.to.title.no_opus}") 	protected String   NO_PARSE_OPUS_PREFIX;
-	/** 특정패턴의 품번을 고정스튜디오로 */	@Value("${parse.to.title.re_opus}") 	protected String[]  REPLACE_OPUS_INFO;
+    /* Common Properties */
 
-	/** 본 비디오 이돌 */ 					@Value("${batch.watched.moveVideo}")	protected boolean       MOVE_WATCHED_VIDEO;
-	/** rank가 낮은 비디오 삭제 */ 			@Value("${batch.rank.deleteVideo}") 	protected boolean  DELETE_LOWER_RANK_VIDEO;
-	/** score가 낮은 비디오 삭제 */ 		@Value("${batch.score.deleteVideo}") 	protected boolean DELETE_LOWER_SCORE_VIDEO;
-	
+    /** minimum rank in properties */            @Value("${rank.minimum}")               protected Integer  MIN_RANK;
+    /** maximum rank in properties */            @Value("${rank.maximum}")               protected Integer  MAX_RANK;
+    /** baseline rank */                                                                 protected int     BASE_RANK = 0;
 
-	/** play 배점 */ 						@Value("${score.ratio.play}")			protected int      PLAY_RATIO;
-	/** rank 배점 */ 						@Value("${score.ratio.rank}")			protected int      RANK_RATIO;
-	/** 배우 배점 */ 						@Value("${score.ratio.actress}")		protected int   ACTRESS_RATIO;
-	/** 자막 배점 */ 						@Value("${score.ratio.subtitles}")		protected int SUBTITLES_RATIO;
-	
-	/** 새 비디오 구독 RSS url */ 			@Value("${url.rss}") 					protected URL rssUrl;
-	
-	/** 비디오 정보 찾기 url */ 			@Value("${url.search.video}") 			protected String urlSearchVideo;
-	/** 배우 정보 찾기 url */				@Value("${url.search.actress}") 		protected String urlSearchActress;
-	/** 토렌트 정보 찾기 url*/				@Value("${url.search.torrent}") 		protected String urlSearchTorrent;
+    /** maximum size of entire video */          @Value("${size.video.storage}")         protected int      MAX_ENTIRE_VIDEO;
+
+    /** specific opus of non studio */           @Value("${parse.to.title.no_opus}")     protected String   NO_PARSE_OPUS_PREFIX;
+    /** replacement opus */                      @Value("${parse.to.title.re_opus}")     protected String[] REPLACE_OPUS_INFO;
+
+    /** option for moving video */               @Value("${batch.watched.moveVideo}")    protected boolean        MOVE_WATCHED_VIDEO;
+    /** option for deleting lower rank vidoe */  @Value("${batch.rank.deleteVideo}")     protected boolean   DELETE_LOWER_RANK_VIDEO;
+    /** option for deleting lower score video */ @Value("${batch.score.deleteVideo}")    protected boolean  DELETE_LOWER_SCORE_VIDEO;
+
+    /** play ratio for score */                  @Value("${score.ratio.play}")           protected int      PLAY_RATIO;
+    /** rank ratio for score */                  @Value("${score.ratio.rank}")           protected int      RANK_RATIO;
+    /** actress ratio for score */               @Value("${score.ratio.actress}")        protected int   ACTRESS_RATIO;
+    /** subtitles  ratio for score */            @Value("${score.ratio.subtitles}")      protected int SUBTITLES_RATIO;
+
+    /** new video RSS feed */                    @Value("${url.rss}")                    protected URL rssUrl;
+
+    /** video info finding url */                @Value("${url.search.video}")           protected String urlSearchVideo;
+    /** actress info finding url */              @Value("${url.search.actress}")         protected String urlSearchActress;
+    /** torrent info finding url*/               @Value("${url.search.torrent}")         protected String urlSearchTorrent;
 
 }
